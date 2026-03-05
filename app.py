1ad97963e2563deccf988a5ab31b54480e6fa010
HayderFatlawi <90522970+HayderFatlawi@users.noreply.github.com>
2026-03-05 20:31:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -198,6 +198,8 @@ def check_models():
     # نرسل الحالة لكل مدينة (True يعني Model Ready، False يعني Needs Training)
     return jsonify({"models": status, "all_ready": all_ready})
 
-if __name__ == "__main__":
+import os
 
-    app.run(debug=False, use_reloader=False, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
